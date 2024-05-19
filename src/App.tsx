0b71b067098a10f510040bc8c81186315773a8b0
xBookbig <koonbookza2015@gmail.com>
2024-05-20 00:51:28 +0700

    add () etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import HomePage from './components/home/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/shape/Shape.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Shape.scss'
+
+function Shape() {
+  return (
+    <div>Shape</div>
+  )
+}
+
+export default Shape
